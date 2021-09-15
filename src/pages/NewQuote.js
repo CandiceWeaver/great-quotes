bcf876621599a8f743bbd9d4cf794825687eece3
Candice Weaver <68248078+CandiceWeaver@users.noreply.github.com>
2021-09-15 14:00:15 +0100

    complete

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,11 +1,26 @@
+import { useEffect } from 'react';
+import { useHistory } from 'react-router';
 import QuoteForm from '../components/quotes/QuoteForm';
+import useHttp from '../hooks/use-http';
+import { addQuote } from '../lib/api';
 
 const NewQuote = () => {
+  const { sendRequest, status } = useHttp(addQuote);
+  const history = useHistory();
+
+  useEffect(() => {
+    if (status === 'completed') {
+      history.push('/quotes');
+    }
+  }, [history, status]);
+
   const addQuoteHandler = quoteData => {
-    console.log(quoteData);
+    sendRequest(quoteData);
   };
 
-  return <QuoteForm onAddQuote={addQuoteHandler} />;
+  return (
+    <QuoteForm isLoading={status === 'pending'} onAddQuote={addQuoteHandler} />
+  );
 };
 
 export default NewQuote;
